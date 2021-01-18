da846f84f9568f8b5865cf2d52a3cd4e66bb5765
Kirk Roerig <kirk.roerig@advanced-space.com>
2021-01-18 09:34:25 -0700

    Added test for vec_add_scl

---
+++ tests/vec_add_scl.cpp
@@ -0,0 +1,25 @@
+#include ".test.h"
+
+#include "xmath.h"
+
+#define RAND_F (((random() % 2048) / 1024.f) - 1.f)
+
+TEST
+{
+    xmath::vec<5> a, b, r, expected;
+
+    for (int i = 5; i--;)
+    {
+        a[i] = RAND_F;
+        b[i] = RAND_F;
+        expected[i] = a[i] + b[i] * 5;
+    }
+
+    //vec_add(5, r, a, b);
+    VEC_ADD_SCL(5, r.v, a.v, b.v, 5);
+
+    for (int i = 5; i--;)
+    {
+        assert(fabs(expected[i] - r[i]) < 1e-8);
+    }
+}
