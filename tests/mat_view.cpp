9d1af34606b4e32aad759f0175a7d40f03022fe3
Kirk Roerig <mr.possoms@gmail.com>
2022-06-04 18:06:40 -0400

    Fixed mat_view test

--- tests/mat_view.cpp
+++ tests/mat_view.cpp
@@ -5,12 +5,12 @@
 
 TEST
 {
-    auto M = xmath::mat<4, 4>::look_at({1, 33, 7}, {0, 0, -1}, {0, 1, 0});
+    auto M = xmath::mat<4, 4>::look_at({1, 33, 7}, {0, 0, 1}, {0, 1, 0});
 
     auto p = M.transpose() * xmath::vec<3>{0, 0, 0};
 
     std::cerr << p.to_string() << "\n";
-    assert(p.is_near({1, 33, 7}));
+    assert(p.is_near({-1, 33, 7}));
 
     return 0;
 }
